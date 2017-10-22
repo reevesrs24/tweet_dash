09cd23e031d11dba541322dbdf37ea284c2e05c5
pip <reevesrs24@gmail.com>
2017-10-22 02:55:35 -0700

    added modal

--- tweet_dash/tweet_dash/views.py
+++ tweet_dash/tweet_dash/views.py
@@ -1,6 +1,20 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
-
+from firebase import firebase
+import time
+import datetime
 
 def index(request):
-    return render(request, 'index.html')
+    f = firebase.FirebaseApplication('https://tweet-dash.firebaseio.com', None)
+    result = f.get('/tweet-dash', None)
+    tweets = []
+    for res in result.values():
+      timestamp = time.mktime(time.strptime(res['date'], '%Y-%m-%d %H:%M:%S'))
+      res['date'] = timestamp
+      tweets.append(res)
+    tweets.sort(key=lambda x: x['date'], reverse=True)
+    for tweet in tweets:
+      tweet['day'] = datetime.datetime.fromtimestamp(int(tweet['date'])).strftime("%A")
+      date = datetime.datetime.fromtimestamp(int(tweet['date'])).strftime('%Y-%m-%d %H:%M:%S')
+      tweet['date'] = date
+    return render(request, 'index.html', {'tweets': tweets})
