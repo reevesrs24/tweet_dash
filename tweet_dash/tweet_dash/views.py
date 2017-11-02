d8362292b1dd6b8511a5bb4f01ef7d7f9a7f191c
pip <reevesrs24@gmail.com>
2017-11-02 11:48:36 -0700

    switched twitter icons

--- tweet_dash/tweet_dash/views.py
+++ tweet_dash/tweet_dash/views.py
@@ -11,12 +11,12 @@ def index(request):
     result = r.json()
     tweets = []
     for res in result.values():
-      timestamp = time.mktime(time.strptime(res['date'], '%Y-%m-%d %H:%M:%S'))
+      timestamp = time.mktime(time.strptime(res['date'], '%Y-%m-%dT%H:%M:%S'))
       res['date'] = timestamp
       tweets.append(res)
     tweets.sort(key=lambda x: x['date'], reverse=True)
     for tweet in tweets:
       tweet['day'] = datetime.datetime.fromtimestamp(int(tweet['date'])).strftime("%A")
-      date = datetime.datetime.fromtimestamp(int(tweet['date'])).strftime('%Y-%m-%d %H:%M:%S')
+      date = datetime.datetime.fromtimestamp(int(tweet['date'])).strftime('%Y-%m-%dT%H:%M:%S')
       tweet['date'] = date
     return render(request, 'index.html', {'tweets': tweets})
